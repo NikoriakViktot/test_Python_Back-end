7bde0357ba49c891bff8e760bee45c8952e03bb3
NikoriakViktot <83222767+NikoriakViktot@users.noreply.github.com>
2021-12-17 16:11:06 +0200

    baza_city

--- api_sglite.py
+++ api_sglite.py
@@ -1,3 +1,7 @@
+import collections
+import sys
+import traceback
+
 from config import API_KEY
 import requests
 from bs4 import BeautifulSoup
@@ -11,33 +15,15 @@ import sqlite3 as sq
 # Таблиці в базі даних повинні мати такі стовбці:
 # date, temp(середня температура за добу в градусах цельсія),
 # pcp (опади за день), clouds, pressure, humidity, wind_speed.
-
-url = 'https://time-in.ru/coordinates/ukraine'
-def requests_city(url):
-    # url = 'https://time-in.ru/coordinates/ukraine'
-    get_city = requests.get(url=url)
-    content = get_city.content
-    soup_city = BeautifulSoup(content, "lxml")
-    ul_city = soup_city.find(class_="coordinates-items")
-    city = [i for i in zip([''.join(i) for i in
-            [re.findall('[А-Яа-я]', str(i) ) for i
-            in ul_city.find_all(class_="coordinates-items-left")]],
-            [i for i in [re.findall('\d{2,}.\d{1,}..\d{2,}.\d{1,}|\d{2,}', str(i))
-            for i in ul_city.find_all(class_ = "coordinates-items-right")]])]
-    return city
-
-requests_city(url)
-
-with sq.connect('city.db') as con:
+with sq.connect('city_weather.db') as con:
     cur = con.cursor()
     cur.execute('''CREATE TABLE IF NOT EXISTS city
                    (id_city INTEGER PRIMARY KEY,
                    city TEXT NOT NULL,
                    lat REAL NOT NULL,
                    lon REAL NOT NULL)''')
-
     cur.execute('''CREATE TABLE IF NOT EXISTS forecast
-                       (id INTEGER PRIMARY KEY,
+                       (id INTEGER PRIMARY KEY AUTOINCREMENT,
                        date INTEGER NOT NULL,
                        temp REAL NOT NULL,
                        pcp REAL NOT NULL,
@@ -47,14 +33,74 @@ with sq.connect('city.db') as con:
                        wind_speed REAL NOT NULL)''')
     con.commit()
 
+    url = 'https://time-in.ru/coordinates/ukraine'
+    def requests_city(url):
+        get_city = requests.get(url=url)
+        content = get_city.content
+        soup_city = BeautifulSoup(content, "lxml")
+        patern_city = re.compile('[А-Яа-я\b]|[А-Яа-я\b\sА-яа-я\b]')
+        patern_coord = re.compile('(\d{2,}.\d{1,})..(\d{2,}.\d{1,}|\d{2,})')
+        list_city = [''.join(i) for i in[re.findall(patern_city, str(i))
+                      for i in soup_city.find_all(class_="coordinates-items-left")]]
+        list_coord =  [i for i in [re.findall(patern_coord, str(i))
+                        for i in soup_city.find_all(class_ = "coordinates-items-right")]]
+        city_list = zip(enumerate(list_city,1),list_coord)
+        city = collections.namedtuple('city', ['id', 'name', 'lat', 'lon'])
+        for i in city_list:
+            id = i[0][0]
+            city_name = i[0][1].strip()
+            if not city_name == "":
+                lat = i[1][0][0]
+                lon = i[1][0][1]
+                yield city(int(id),str(city_name),float(lat),float(lon))
 
+    # city = [i for i in zip([''.join(i) for i in
+    #         [re.findall(patern_city, str(i)) for i
+    #         in soup_city.find_all(class_="coordinates-items-left")]],
+    #         [i for i in [re.findall(patern_coord, str(i))
+    #         for i in soup_city.find_all(class_ = "coordinates-items-right")]])]
+
+    def save_city_db(city):
+        for i in city:
+            data = i[:]
+            print(data)
+            cur.execute( ''' INSERT INTO city
+                         (id_city, city, lat, lon) 
+                         VALUES(?,?,?,?)''', data)
+            con.commit()
+
+
+
+            # except sq.Error as error:
+            #     print("Не удалось вставить данные в таблицу sqlite")
+            #     print("Класс исключения: ", error.__class__)
+            # finally:
+            #     if (cur):
+            #         cur.close()
+            #         print("Соединение с SQLite закрыто")
+
+
+
+
+            # cur.executemany("insert into characters(c) values (?)", char_generator())
+            #
+            # cur.execute("select c from characters")
+            # print(cur.fetchall())
+
+
+
+
+
+
+    # requests_city(url)
+    save_city_db(requests_city(url))
 
 
 DATA = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=&appid={API_KEY}&units=metric&lang=ua")
 
 t = DATA.json().get('weather')
 # p = DATA.json().get('minutely')
-c = DATA.json().get('clouds')
+c = DATA.json().get('daily')
 pr = DATA.json().get('daily')
 h = DATA.json().get('humidity')
 w = DATA.json().get('wind_speed')
